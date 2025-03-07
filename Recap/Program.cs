fa636c2937f082acb0925beda38562b303374de2
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-03-07 23:55:13 +0300

    Создал программу с массивом внутри кортежа

---
+++ Recap/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.WebSockets;
+
+namespace Recap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = (name: "", dishes: new string[5]);
+            Console.WriteLine("Как тебя зовут?");
+            user.name = Console.ReadLine();
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Напишите свое любимое блюдо №{i + 1}");
+                user.dishes[i] = Console.ReadLine();
+            }
+        }
+    }
+}
