cfdf04b73bc040113059f675fa826d5e543af5c0
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-03-08 00:44:05 +0300

    Создал метод для создания массива с вводимым количеством элементов массива и метод для сортировки массива

---
+++ ArrayMethods/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ArrayMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите количество элементов в массиве: ");
+            
+            int[] myArray = GetArrayFromConsole(Convert.ToInt32(Console.ReadLine()));
+            SortArray(myArray);
+            Console.WriteLine("");
+            foreach (var value in myArray)
+            {
+                Console.WriteLine(value);
+            }
+        }
+        static int[] GetArrayFromConsole(int valuesNumber)
+        {
+            int[] values = new int[valuesNumber];
+            for (int i = 0; i < valuesNumber; i++)
+            {
+                Console.WriteLine($"Введите значение массива №{i}: ");
+                values[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return values;
+        }
+        static int[] SortArray(int[] array)
+        {
+            int temp = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] < array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+
+    }
+}
---
+++ ShowColor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShowColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
