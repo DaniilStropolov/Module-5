53a0f55b2fbf656817fc8dcf04a5cf017593d9a5
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-03-13 00:59:28 +0300

    Переделал программу. Теперь существует метод, который вызывает два метода сортировки в разные стороны

--- SortArrayAscDesc/Program.cs
+++ SortArrayAscDesc/Program.cs
@@ -11,26 +11,36 @@
                 Console.WriteLine(num);
             }
 
-            Console.WriteLine("Отсортированный по возрастанию массив: ");
-            SortArrayAsc(numbers);
-            foreach (int num in numbers)
+            int[] sortedArrayAsc;
+            int[] sortedArrayDesc;
+
+            SortArray(numbers, out sortedArrayAsc, out sortedArrayDesc);
+
+            Console.WriteLine("Массив, отсортированный по возрастанию: ");
+            foreach (int num in sortedArrayAsc)
             {
                 Console.WriteLine(num);
             }
 
-            Console.WriteLine("Отсортированный по убыванию массив: ");
-            SortArrayDesc(numbers);
-            foreach (int num in numbers)
+            Console.WriteLine("Массив, отсортированный по убыванию: ");
+            foreach (int num in sortedArrayDesc)
             {
                 Console.WriteLine(num);
             }
         }
-        static int[] SortArrayDesc(int[] numbers)
+        static void SortArray(int[] numbers, out int[] sortedArrayAsc, out int[] sortedArrayDesc)
+        {
+            sortedArrayAsc = (int[])numbers.Clone();
+            sortedArrayDesc = (int[])numbers.Clone();
+            SortArrayAsc(sortedArrayAsc);
+            SortArrayDesc(sortedArrayDesc);
+        }
+        static void SortArrayDesc(int[] numbers)
         {
             int temp = 0;
-            for (int i = 0; i < numbers.Length; i ++)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                for (int j = 0; j < numbers.Length; j ++)
+                for (int j = 0; j < numbers.Length; j++)
                 {
                     if (numbers[i] > numbers[j])
                     {
@@ -40,9 +50,8 @@
                     }
                 }
             }
-            return numbers;
         }
-        static int[] SortArrayAsc(int[] numbers)
+        static void SortArrayAsc(int[] numbers)
         {
             int temp = 0;
             for (int i = 0; i < numbers.Length; i++)
@@ -57,7 +66,6 @@
                     }
                 }
             }
-            return numbers;
         }
     }
 }
