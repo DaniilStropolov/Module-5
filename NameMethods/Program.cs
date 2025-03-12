66ee336ccb91757ba9ebb95742d00b527937fd11
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-03-12 19:50:45 +0300

    Создал программу с методами для сортировки массива по возрастанию и убыванию

---
+++ NameMethods/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace NameRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Евгений";
+            int age = 10;
+            
+            Console.WriteLine(name + " " + age);
+            ChangeName(ref name);
+            ChangeAge(ref age);
+            Console.WriteLine(name + " " + age);
+        }
+        static void ChangeName(ref string name)
+        {
+            Console.WriteLine("Введите имя: ");
+            name = Console.ReadLine();
+        }
+        static void ChangeAge(ref int age)
+        {
+            Console.WriteLine("Введите возраст: ");
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
---
+++ SortArrayAscDesc/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace SortArrayAscDesc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 6, 2, 7, 9, 3, 6, 5, 22, -2 };
+            Console.WriteLine("Первоначальный массив: ");
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("Отсортированный по возрастанию массив: ");
+            SortArrayAsc(numbers);
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.WriteLine("Отсортированный по убыванию массив: ");
+            SortArrayDesc(numbers);
+            foreach (int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+        static int[] SortArrayDesc(int[] numbers)
+        {
+            int temp = 0;
+            for (int i = 0; i < numbers.Length; i ++)
+            {
+                for (int j = 0; j < numbers.Length; j ++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+            return numbers;
+        }
+        static int[] SortArrayAsc(int[] numbers)
+        {
+            int temp = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (numbers[i] < numbers[j])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+            return numbers;
+        }
+    }
+}
