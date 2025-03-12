8ea0de16d093f5054aa23bf2f301f40ca017a938
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-03-13 02:14:30 +0300

    Создал программу для возведения числа в степень через рекурсивный метод

---
+++ PowerUp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PowerUp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите степень");
+            byte pow = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine(PowerUp(N, pow));
+        }
+        private static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
+            }
+        }
+    }
